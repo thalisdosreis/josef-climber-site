96b0a185ba05d9c01125611d0bc1ebabddaf480e
thalisdosreis <thalisreis000@gmail.com>
2026-07-07 14:29:38 -0400

    Teste para saber se as imagens do Dr. Mário foram 5.0

--- pages/index.js
+++ pages/index.js
@@ -735,7 +735,7 @@ const sampleListings = [
     location: "Bairro Da Paz - Manaus AM",
     mapLink: "https://maps.app.goo.gl/pe2vGFj12oJxUPF4A",
     images: [
-    "/casa.drmário (9).jpeg",
+    "/casa-drmario-9.jpeg",
     "/casa.drmário (8).jpeg",
     "/casa.drmário (2).jpeg",
     "/casa.drmário (3).jpeg",
@@ -743,7 +743,7 @@ const sampleListings = [
     "/casa.drmário (5).jpeg",
     "/casa.drmário (4).jpeg",
     "/casa.drmário (6).jpeg",
-    "/casa.drmário1.jpeg (1599x899)"
+    "/casa.drmário1.jpeg"
    
     ]
   },
